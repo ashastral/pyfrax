b8da392d452ac36f9e27460d7381796a61b723a0
Fraxtil <github@xtil.net>
2012-01-13 22:55:41 -0700

    Fixed bugs, added an example 'cube.py'

--- pyfrax.py
+++ pyfrax.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python
 
-class 3d:
-    def __init__(width, height, color):
-        ps = pixel_size(color)
+class canvas_3d:
+    def __init__(self, width, height, color):
+        ps = self.pixel_size(color)
         if ps == False:
             raise ValueError("unsupported color")
         self.ps = ps
         self.image = bytearray('\x00' * (width * height * len(color)))
         self.vanishing_point(width / 2, height / 2)
         self.dim = (width, height)
-    def pixel_size(color):
+    def pixel_size(self, color):
         if color in ("rgb", "rgba", "k", "ka"):
             return len(color)
         return False
-    def vanishing_point(x, y):
+    def vanishing_point(self, x, y):
         self.vp = (x, y)
-    def point(x, y, z, c):
+    def point(self, x, y, z, c):
         if len(c) != self.ps:
             raise ValueError("invalid color")
-        fp = flatten_point(x, y, z)
+        fp = self.flatten_point(x, y, z)
         # out of bounds check
-        if not all([0 > fp[n] > self.dim[n] for n in xrange(2)]):
+        if not all([0 < fp[n] < self.dim[n] for n in xrange(2)]):
+            print "out of bounds @ %s,%s" % tuple(fp)
             return
-        c = shade(x, y, z, c)
-        pixel_start = fp[0] + fp[1] * self.dim[0]
+        c = self.shade(x, y, z, c)
+        pixel_start = int(fp[0]) + int(fp[1]) * self.dim[0]
         self.image[pixel_start:pixel_start + self.ps] = c
-    def flatten_point(x, y, z):
+    def flatten_point(self, x, y, z):
         if z <= 0:
             raise ValueError("invalid depth")
         distance = (x - self.vp[0], y - self.vp[1])
         distance = [d / z for d in distance]
-        return (vp + d for vp, d in self.vp, distance)
-    def shade(x, y, z, c):
+        return [vp + d for vp, d in zip(self.vp, distance)]
+    def shade(self, x, y, z, c):
         return c
-    def write(filename):
+    def write(self, filename):
         with open(filename, 'wb') as fh:
             fh.write(str(self.image))
         
